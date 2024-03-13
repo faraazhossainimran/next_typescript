c651dfc0c0b31fbcbed0390a3b913baec59edba1
Imran Hossain <faraazhossainimran@gmail.com>
2024-03-14 03:05:11 +0600

    navbar, footer added

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -2,9 +2,9 @@
 
 import { customButtonProps } from "@/types"
 
-const CustomButton = ({title, containerStyles, handleClick}: customButtonProps) => {
+const CustomButton = ({title, containerStyles, handleClick, btnType}: customButtonProps) => {
     return (
-        <button disabled={false} type={"button"} className={`custom-btn`} onClick={()=> {}}>
+        <button disabled={false} type={btnType || "button"} className={`custom-btn`} onClick={()=> {}}>
             <span className={`flex-1 ${containerStyles}`} onClick={handleClick}>
                 {title}
             </span>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,7 @@
 "use client"
 import React from "react";
 import { CustomButton } from ".";
+import Image from "next/image";
 
 const Hero = () => {
     const handleScroll = () => {
@@ -8,8 +9,9 @@ const Hero = () => {
     }
   return (
     <div className="hero">
-      <div className="flex-1 pt-36 padding-x">
-        <h1 className="hero__title">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 flex-1 pt-36 padding-x">
+            <div>
+            <h1 className="hero__title">
           Find, book, or rent a car - quickly and easily!
         </h1>
         <p className="hero__subtitle">
@@ -21,6 +23,13 @@ const Hero = () => {
           containerStyles="bg-primary-blue text-white rounded-full mt-10 py-2 px-4"
           handleClick = {()=> {handleScroll}}
         />
+            </div>
+        <div className="hero__image-container">
+            <div className="hero__image">
+                <Image src="/hero.png" alt="Hero" className="object-container" fill />
+            </div>
+            <div className="hero__image-overlay"/>
+        </div>
       </div>
     </div>
   );
